ed7cb991393b99f9e190582a9b8fc6bc6fb05dc9
Sunny <SunnyRaj@users.noreply.github.com>
2016-01-28 15:34:27 -0500

    Revamp scripts

--- setup_home.sh
+++ setup_home.sh
@@ -1,45 +1,32 @@
 #! /bin/sh
 
+##Install grub customizer
 
-sudo apt-get install -y build-essential tmux git vim curl zsh gparted
+sudo apt-get install -y software-properties-common python-software-properties
+sudo add-apt-repository -y ppa:danielrichter2007/grub-customizer
+sudo apt-get update && sudo apt-get install -y grub-customizer
 
-mkdir Projects && sudo wget -O - https://github.com/sunnyraj/vimrc/raw/master/autoinstall.sh | sh
+sudo apt-get install -y build-essential tmux git vim curl zsh gparted ctags ruby rake
 
+##Setup tmux configuration
+touch .tmux.conf
+cp tmux.conf ~/.tmux.conf
+
+#Install Oh My Zsh shell
 curl -L http://install.ohmyz.sh | sh
 
 ##Adding aliases:
-
 touch .zsh_aliases
-
-echo "
-
-alias nimbus=\"ssh -X sundarcs@nimbus.seas.gwu.edu\"
-
-
-## Colorize the ls output
-alias ls=\'ls --color=auto\'
-
-## Use a long listing format ##
-alias ll=\'ls -la\'
-
-## Show hidden files ##
-alias l.=\'ls -d .* --color=auto\'
-
-" >> .zsh_aliases
 
 # Load the alias file
 echo ". ~/.zsh_aliases" >> .zshrc
 
+# Install Janus vim
+curl -L https://bit.ly/janus-bootstrap | bash
+
+cd .vim/janus/vim/colors/
+git clone https://github.com/SunnyRaj/cobalt2-vim-theme.git
+cd -
+
 ## Make zsh your default shell. Do not use sudo
-
 chsh -s /bin/zsh
-
-##Install grub customizer
-
-sudo apt-get install -y software-properties-common python-software-properties && sudo add-apt-repository ppa:danielrichter2007/grub-customizer && sudo apt-get update && sudo apt-get install -y grub-customizer
-
-##Setup tmux configuration
-
-touch .tmux.conf
-
-cp tmux.conf ~/.tmux.conf

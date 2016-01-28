3d642a7f55c4d71401debfaa2d0bff0593539c63
Sunny <SunnyRaj@users.noreply.github.com>
2016-01-28 16:03:09 -0500

    Install powerline fonts

--- setup_home.sh
+++ setup_home.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y software-properties-common python-software-properties
 sudo add-apt-repository -y ppa:danielrichter2007/grub-customizer
 sudo apt-get update && sudo apt-get install -y grub-customizer
 
-sudo apt-get install -y build-essential tmux git vim curl zsh gparted ctags ruby rake
+sudo apt-get install -y build-essential tmux git vim curl zsh gparted ctags ruby rake python-pip
 
 ##Setup tmux configuration
 touch .tmux.conf
@@ -27,6 +27,15 @@ cp vimrc.after ~/.vimrc.after
 cd .vim/janus/vim/colors/
 git clone https://github.com/SunnyRaj/cobalt2-vim-theme.git
 cd -
+
+## Powerline installation
+sudo pip install git+git://github.com/Lokaltog/powerline
+
+## Powerline Font installation
+wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
+sudo mv PowerlineSymbols.otf /usr/share/fonts/
+sudo fc-cache -vf
+sudo mv 10-powerline-symbols.conf /etc/fonts/conf.d/
 
 ## Make zsh your default shell. Do not use sudo
 chsh -s /bin/zsh

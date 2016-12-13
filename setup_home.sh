9954fe692d2fc3d11242e766a2db7d0af2863488
Sunny Raj <SunnyRaj@users.noreply.github.com>
2016-12-13 08:49:03 -0500

    Update setup_home.sh

--- setup_home.sh
+++ setup_home.sh
@@ -20,7 +20,7 @@ sudo apt-get install -y build-essential git vim curl zsh gparted ctags ruby rake
 #sudo apt-get install -y gcc-6 g++-6
 #sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-6
 cat ~/setup-home-env/zshrc >> ~/.zshrc
-source ~/.zshrc
+. ~/.zshrc
 ##Install latest gnuplot
 brew install gnuplot --with-cairo --with-x11
 
